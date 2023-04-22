3b87d07133c77f361c921602cb9b1bc9ff8cbb6a
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-04-22 20:17:57 +0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
+	base: '/vite/',
 	plugins: [react(), eslint()],
 	css: {
 		devSourcemap: true
